1e1bd48d36d3afee2d751c4eeb193e37c32fea7b
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-13 14:12:46 +0500

    menu link started

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import headerStyle from "./header.module.css";
+import { NavLink } from "react-router-dom";
+import logo from "../../img/Logo/neu.svg";
 
 const Header = () => {
-  return <div className="header-container">
-
-  </div>;
+  return (
+    <div className={headerStyle.headerContainer}>
+      <div className={headerStyle.header}>
+        <img src={logo} alt="logo" />
+        <div className={headerStyle.menuIcon}>
+          <NavLink to={"/about"}>About</NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
