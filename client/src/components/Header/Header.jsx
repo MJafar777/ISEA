26d895a115028112e4ccfc4322fbfe675fa4ba67
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-13 14:34:28 +0500

    link changed settings

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,39 @@
 import React from "react";
-import headerStyle from "./header.module.css";
+import s from "./header.module.css";
 import { NavLink } from "react-router-dom";
 import logo from "../../img/Logo/neu.svg";
 
 const Header = () => {
+  const container = s.header + " " + "container";
   return (
-    <div className={headerStyle.headerContainer}>
-      <div className={headerStyle.header}>
+    <div className={s.main}>
+      <div className={container}>
         <img src={logo} alt="logo" />
-        <div className={headerStyle.menuIcon}>
-          <NavLink to={"/about"}>About</NavLink>
+        <div className={s.menuIcon}>
+          <NavLink className={s.link} to={"/about"}>
+            About
+          </NavLink>
+          <NavLink className={s.link} to={"/ourwork"}>
+            OUR WORK
+          </NavLink>
+          <NavLink className={s.link} to={"/renewables"}>
+            RENEWABLES
+          </NavLink>
+          <NavLink className={s.link} to={"/newroom"}>
+            NEWSROOM
+          </NavLink>
+          <NavLink className={s.link} to={"/publications"}>
+            PUBLICATIONS
+          </NavLink>
+          <NavLink className={s.link} to={"/education"}>
+            EDUCATION
+          </NavLink>
+          <NavLink className={s.link} to={"/data"}>
+            DATA
+          </NavLink>
+          <NavLink className={s.link} to={"/events"}>
+            EVENTS
+          </NavLink>
         </div>
       </div>
     </div>
