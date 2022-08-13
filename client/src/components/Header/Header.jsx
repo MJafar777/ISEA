33637b252e55288b408f58784a826f58e3b7397a
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-13 15:09:25 +0500

    header finished

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import s from "./header.module.css";
+
 import { NavLink } from "react-router-dom";
+
 import logo from "../../img/Logo/neu.svg";
+
+import lang from "../../img/icons/lang.svg";
 
 const Header = () => {
   const container = s.header + " " + "container";
@@ -9,31 +13,41 @@ const Header = () => {
     <div className={s.main}>
       <div className={container}>
         <img src={logo} alt="logo" />
-        <div className={s.menuIcon}>
-          <NavLink className={s.link} to={"/about"}>
-            About
-          </NavLink>
-          <NavLink className={s.link} to={"/ourwork"}>
-            OUR WORK
-          </NavLink>
-          <NavLink className={s.link} to={"/renewables"}>
-            RENEWABLES
-          </NavLink>
-          <NavLink className={s.link} to={"/newroom"}>
-            NEWSROOM
-          </NavLink>
-          <NavLink className={s.link} to={"/publications"}>
-            PUBLICATIONS
-          </NavLink>
-          <NavLink className={s.link} to={"/education"}>
-            EDUCATION
-          </NavLink>
-          <NavLink className={s.link} to={"/data"}>
-            DATA
-          </NavLink>
-          <NavLink className={s.link} to={"/events"}>
-            EVENTS
-          </NavLink>
+        <div className={s.links}>
+          <div className={s.menuIcon}>
+            <NavLink className={s.link} to={"/about"}>
+              ABOUT
+            </NavLink>
+            <NavLink className={s.link} to={"/ourwork"}>
+              OUR WORK
+            </NavLink>
+            <NavLink className={s.link} to={"/renewables"}>
+              RENEWABLES
+            </NavLink>
+            <NavLink className={s.link} to={"/newroom"}>
+              NEWSROOM
+            </NavLink>
+            <NavLink className={s.link} to={"/publications"}>
+              PUBLICATIONS
+            </NavLink>
+            <NavLink className={s.link} to={"/education"}>
+              EDUCATION
+            </NavLink>
+            <NavLink className={s.link} to={"/data"}>
+              DATA
+            </NavLink>
+            <NavLink className={s.link} to={"/events"}>
+              EVENTS
+            </NavLink>
+          </div>
+          <div className={s.selector}>
+            <img className={s.lang} src={lang} alt="lang" />
+            <select className="select" name="language" id="language">
+              <option value="eng">Eng</option>
+              <option value="rus">Rus</option>
+              <option value="uzb">Uzb</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
