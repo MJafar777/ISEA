b1dcf33211f88cc680db7f764d305db0270f0384
Pulatoff <niyozbekpulatov@gmail.com>
2022-08-13 15:12:34 +0500

    bugs fixed and merged

--- client/src/components/AboutLanding/AboutLanding.jsx
+++ client/src/components/AboutLanding/AboutLanding.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Title from "../title/Title";
+import Title from "../Title/Title";
 import s from "./AboutLanding.module.css";
 import AboutLandingCard from "./AboutLandingCard/AboutLandingCard";
 import image1 from "../../img/hero-Slider/2.jpg";
--- client/src/components/AboutLanding/AboutLandingCard/AboutLandingCard.jsx
+++ client/src/components/AboutLanding/AboutLandingCard/AboutLandingCard.jsx
@@ -1,7 +1,6 @@
 import s from "../AboutLanding.module.css";
 import React from "react";
-import image from "../../../img/hero-Slider/2.jpg";
-import Button from "../../button/Button";
+import Button from "../../Button/Button";
 
 export default function AboutLandingCard({ image, text }) {
   return (
