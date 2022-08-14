48942f48da382a8be51206360166e3489f51a9fb
Pulatoff <niyozbekpulatov@gmail.com>
2022-08-14 17:34:10 +0500

    news routes writed

--- server/middlewares/app.js
+++ server/middlewares/app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const newsRouter = require("../routes/newsRouter");
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/v1/news", newsRouter);
 
 module.exports = app;
