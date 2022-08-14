25966e85c25f055d89adc0a6d2a0368187279815
Pulatoff <niyozbekpulatov@gmail.com>
2022-08-14 16:23:30 +0500

    connecting to database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require("dotenv").config({});
-const express = require("express");
-const app = express();
+const app = require("./middlewares/app");
+const db = require("./configs/db");
+
+// connecting database to server
+db();
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port: ${process.env.PORT}`);
