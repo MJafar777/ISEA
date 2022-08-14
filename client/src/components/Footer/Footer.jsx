960c4384e47202b13f98d5d159a25eb8bbddddb5
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-14 13:32:48 +0500

    footer design finished

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import s from "./footer.module.css";
+
+import Facebook from "../../img/icon-web-prog/facebook.png";
+import Twitter from "../../img/icon-web-prog/Twitter.png";
+import Instagram from "../../img/icon-web-prog/Instagram.png";
 
 const Footer = () => {
+  const prevent = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={s.footer}>
+      <div className={s.foot}>
+        <div className={s.footerTitle}>
+          <h4>NEW IT</h4>
+          <h6>© Energy Sources</h6>
+        </div>
+        <div className={s.footerTitle}>
+          <a href="/">About Us</a>
+          <a href="/">Contact</a>
+          <a href="/">Terms & Conditions</a>
+        </div>
+        <div className={s.footerTitle}>
+          <a href="/">
+            <img src={Facebook} alt="Facebook" />
+            <span>Facebook</span>
+          </a>
+          <a href="/">
+            <img src={Twitter} alt="Twitter" />
+            <span>Twitter</span>
+          </a>
+          <a href="/">
+            <img src={Instagram} alt="Instagram" />
+            <span>Instagram</span>
+          </a>
+        </div>
+        <div className={s.footerTitle}>
+          <h3>Subscribe to our newsletter</h3>
+          <form onSubmit={prevent}>
+            <input maxLength={30} type="text" placeholder="Email Address" />
+            <button>OK</button>
+          </form>
+        </div>
+        <div className={s.footerTitle}>
+          <p>497 Evergreen Rd. Roseville, CA 95673</p>
+          <p>+44 345 678 903</p>
+          <a href="/">mirzaraximovjafar2001@gmail.com</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
