75e92e67bdfb34f9eb23de2da65c0f04cdf44ae3
Pulatoff <niyozbekpulatov@gmail.com>
2022-08-15 19:52:05 +0500

    crud writed

--- server/middlewares/app.js
+++ server/middlewares/app.js
@@ -3,6 +3,16 @@ const app = express();
 const morgan = require("morgan");
 const newsRouter = require("../routes/newsRouter");
 const booksRouter = require("../routes/booksRouter");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  })
+);
+
 app.use(morgan("dev"));
 app.use(express.json());
 
