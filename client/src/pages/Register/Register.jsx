67ca5797f8f62966ff2047e0b3bf2abecfe48643
Pulatoff <niyozbekpulatov@gmail.com>
2022-08-17 21:30:13 +0500

    dd

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import s from "./register.module.css";
 import { url } from "../../configs/config";
+import axios from "axios";
 
 export default function Register() {
+  const emailRef = useRef();
   const [status, setStatus] = useState("");
+
   const enterEmail = async (e) => {
     e.preventDefault();
-    const response = await fetch(url + "/users/signup");
-    const data = response.json();
-    setStatus(data.status);
+    const email = emailRef.current.value;
+    const response = await axios.post(url + "/users/signup", { email });
+    console.log(response);
+    setStatus(response.data.status);
   };
+  console.log(status);
   return (
-    <div>
-      {status === "Success" ? (
-        <form>
-          <input type="email" required name="email" placeholder="email" />
+    <div className={s.container}>
+      {status !== "Success" ? (
+        <form className={s.content}>
+          <input ref={emailRef} type="email" required placeholder="email" />
           <button onClick={enterEmail}>sign up</button>
         </form>
       ) : (
         <form>
-          <input type="email" required name="email" placeholder="email" />
-          <button onClick={enterEmail}>sign up</button>
+          <input type="text" required placeholder="verify" />
+          <button>verify</button>
         </form>
       )}
     </div>
