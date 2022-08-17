85f3b360bd6af42c3dd76c223c21d6bd34431955
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-17 13:57:02 +0500

    our work finished

--- client/src/pages/OurWork/OurWork.jsx
+++ client/src/pages/OurWork/OurWork.jsx
@@ -3,12 +3,205 @@ import React from "react";
 import s from "./ourWork.module.css";
 
 import ourWork from "../../img/hero-Slider/5.jpg";
+import Title from "../../components/Title/Title";
 const OurWork = () => {
   return (
     <div className="container">
+      <Title name={"Our Work"} />
       <div className={s.img}>
         <div className={s.imgBottom}>
           <p>Our Work</p>
+        </div>
+      </div>
+      <div className={s.main}>
+        <div className={s.leftContainer}>
+          <p>
+            Основной целью организации является содействие эффективному
+            использованию передовых технологий в области солнечной энергетики на
+            основе подготовки и переподготовки кадров в соответствии с
+            международными стандартами и требованиями, стратегический анализ
+            крупных проектов и управление проектами, реализуемых за счет
+            внутренних и иностранных инвестиций, а также содействие в реализации
+            работ направленных на трансфер технологий.
+          </p>
+          <p>
+            With a mandate from countries around the world, ISEA encourages
+            governments to adopt enabling policies for renewable energy
+            investments, provides practical tools and policy advice to
+            accelerate renewable energy deployment, and facilitates knowledge
+            sharing and technology transfer to provide clean, sustainable energy
+            for the world’s growing population.
+          </p>
+          <p>
+            Для достижения своих целей организация выполняет следующие задачи:
+          </p>
+
+          <ul style={{ listStyleType: "circle" }}>
+            <li>
+              проведение фундаментальных, прикладных и инновационных
+              исследований в области <span>солнечной энергии</span>;
+            </li>
+            <li>
+              внедрение целевых научных программ на уровне мировых требований и
+              стандартов, содействие в организации типовых{" "}
+              <span>научно-исследовательских институтов</span>;
+            </li>
+            <li>
+              содействие в организации и организация сертификационных услуг
+              солнечных технологий и их компонентов <span>компонентов</span>;
+            </li>
+            <li>
+              <span>содействие в организации</span> - проектно-изыскательских
+              работ с участием иностранных специалистов;
+            </li>
+            <li>
+              The <span>Global</span>- содействие в разработке и подготовке
+              технико-экономических обоснований (ТЭО) и предварительных
+              технико-экономических обоснований (ПТЭО) наряду с реализацией
+              местных и инвестиционных проектов по внедрению солнечных
+              технологий;
+            </li>
+            <li>
+              содействие в разработке передовых технологий и технических решений
+              солнечной и ветровой энергии на основе передовых материалов;
+            </li>
+            <li>
+              развитие эффективных форм сотрудничества, интеграция науки и
+              техники.
+            </li>
+            <li>
+              содействие укреплению потенциала науки и инноваций путем
+              организации научных конференций, семинаров и практикумов с
+              местными и зарубежными университетами, учебными и
+              научно-исследовательскими учреждениями;
+            </li>
+            <li>
+              содействие развитию международного научно-технического
+              сотрудничества через международные научно-исследовательские и
+              образовательные учреждения, инновационные центры, зарубежных
+              партнеров, научно-технические и инновационные проекты, обмен
+              знаниями и специалистами и реализацию совместных проектов;
+            </li>
+            <li>
+              привлечение грантовых средств из международных проектов и фондов
+              для реализации крупных исследовательских проектов;
+            </li>
+          </ul>
+          <p>
+            организация и проведение конкурсов, конкурсов и конкурсов по
+            солнечным энергетическим технологиям, проблемам и решениям.
+          </p>
+        </div>
+        <div className={s.main2}>
+          <div className={s.context}>
+            <h2>Lorem, ipsum dolor</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+            tenetur eaque alias voluptate autem at! Natus repudiandae numquam
+            explicabo harum eveniet eos soluta, fuga ut, et, eligendi debitis
+            reprehenderit dolorum vero amet nulla dolor tempore earum provident
+            quia! Commodi adipisci nam quaerat molestias, quidem ipsum iusto
+            excepturi perspiciatis maiores, sit quos alias. Similique voluptatum
+            at exercitationem architecto cupiditate harum quo nostrum dolorum
+            vel autem, ab necessitatibus, culpa assumenda? Debitis, cumque
+            suscipit! Quos sit vel pariatur, provident voluptate, mollitia
+            officiis porro blanditiis deleniti tempore consequatur aspernatur
+            earum, natus alias! Modi vitae similique eum distinctio! Neque
+            beatae architecto nam eos minus culpa officia amet quam obcaecati?
+            Doloremque placeat facere quia fugiat repellendus consequatur, quas
+            provident. Eum nihil tempora, hic alias possimus reprehenderit.
+            Recusandae commodi odio inventore quisquam. Officiis repellat ad
+            esse beatae. Sed, possimus cum sequi dignissimos tempore dolore
+            debitis nesciunt perferendis, recusandae error quam laudantium
+            incidunt optio quod ipsa. Omnis similique a repellat sed aut quaerat
+            quas hic debitis. Mollitia perspiciatis delectus pariatur aut
+            nesciunt iusto veritatis quaerat quia vero dolore enim quisquam
+            exercitationem, hic accusantium distinctio saepe minima fugiat illo
+            eveniet quam architecto necessitatibus ipsum, nisi illum! Quod, non
+            aperiam possimus quis iste ullam doloribus commodi ipsa odio!
+            Explicabo laudantium nam cumque quo placeat iure maiores
+            accusantium, repudiandae magnam voluptas quia distinctio rem
+            eligendi cum qui accusamus dolores blanditiis facilis facere, in
+            odio nobis obcaecati magni et. Aperiam vero dignissimos aliquam,
+            error at eos vitae, debitis similique nemo enim maxime quo dolorem
+            ipsam aliquid eveniet voluptate voluptatum incidunt deserunt natus
+            provident. Tempore maxime ducimus natus minima impedit quasi,
+            eligendi unde eius dolore quis. Distinctio, totam architecto! Fugiat
+            rem voluptatum odio! Sit exercitationem expedita eius, enim quis
+            corrupti ipsam assumenda voluptatem ad cupiditate voluptates error
+            nam nobis fugit, quidem mollitia odio voluptatibus eaque impedit.
+          </div>
+          
+          <div className={s.content2}>
+            <div>
+              <h3>lorem</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Pariatur doloribus veniam ullam consequuntur? Autem iste dicta
+                nostrum officiis unde quia!
+              </p>
+            </div>
+            <div>
+              <h3>lorem</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Pariatur doloribus veniam ullam consequuntur? Autem iste dicta
+                nostrum officiis unde quia!
+              </p>
+            </div>
+            <div>
+              <h3>lorem</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Pariatur doloribus veniam ullam consequuntur? Autem iste dicta
+                nostrum officiis unde quia!
+              </p>
+            </div>
+            <div>
+              <h3>lorem</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Pariatur doloribus veniam ullam consequuntur? Autem iste dicta
+                nostrum officiis unde quia!
+              </p>
+            </div>
+          </div>
+          <div className={s.context}>
+            <h2>Lorem, ipsum dolor</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+            tenetur eaque alias voluptate autem at! Natus repudiandae numquam
+            explicabo harum eveniet eos soluta, fuga ut, et, eligendi debitis
+            reprehenderit dolorum vero amet nulla dolor tempore earum provident
+            quia! Commodi adipisci nam quaerat molestias, quidem ipsum iusto
+            excepturi perspiciatis maiores, sit quos alias. Similique voluptatum
+            at exercitationem architecto cupiditate harum quo nostrum dolorum
+            vel autem, ab necessitatibus, culpa assumenda? Debitis, cumque
+            suscipit! Quos sit vel pariatur, provident voluptate, mollitia
+            officiis porro blanditiis deleniti tempore consequatur aspernatur
+            earum, natus alias! Modi vitae similique eum distinctio! Neque
+            beatae architecto nam eos minus culpa officia amet quam obcaecati?
+            Doloremque placeat facere quia fugiat repellendus consequatur, quas
+            provident. Eum nihil tempora, hic alias possimus reprehenderit.
+            Recusandae commodi odio inventore quisquam. Officiis repellat ad
+            esse beatae. Sed, possimus cum sequi dignissimos tempore dolore
+            debitis nesciunt perferendis, recusandae error quam laudantium
+            incidunt optio quod ipsa. Omnis similique a repellat sed aut quaerat
+            quas hic debitis. Mollitia perspiciatis delectus pariatur aut
+            nesciunt iusto veritatis quaerat quia vero dolore enim quisquam
+            exercitationem, hic accusantium distinctio saepe minima fugiat illo
+            eveniet quam architecto necessitatibus ipsum, nisi illum! Quod, non
+            aperiam possimus quis iste ullam doloribus commodi ipsa odio!
+            Explicabo laudantium nam cumque quo placeat iure maiores
+            accusantium, repudiandae magnam voluptas quia distinctio rem
+            eligendi cum qui accusamus dolores blanditiis facilis facere, in
+            odio nobis obcaecati magni et. Aperiam vero dignissimos aliquam,
+            error at eos vitae, debitis similique nemo enim maxime quo dolorem
+            ipsam aliquid eveniet voluptate voluptatum incidunt deserunt natus
+            provident. Tempore maxime ducimus natus minima impedit quasi,
+            eligendi unde eius dolore quis. Distinctio, totam architecto! Fugiat
+            rem voluptatum odio! Sit exercitationem expedita eius, enim quis
+            corrupti ipsam assumenda voluptatem ad cupiditate voluptates error
+            nam nobis fugit, quidem mollitia odio voluptatibus eaque impedit.
+          </div>
         </div>
       </div>
     </div>
