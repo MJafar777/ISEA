c62f80b5f34be3509e7aeecb55b7646249e16ef1
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-18 11:09:13 +0500

    benefits finished

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Renewables from "./pages/Renewables/Renewables";
 import "./pages/Renewables/renewables.module.css";
 import PublicationBook from "./pages/Publication/PublicationBook/PublicationBook";
 import Slider from "./components/Slider/Slider";
+import Benefits from "./components/RenewablesComponents/BENEFITS/Benefits";
 
 const App = () => {
   return (
@@ -28,6 +29,10 @@ const App = () => {
         {/* about page child comonents finished */}
         <Route path="/ourwork" element={<OurWork />} />
         <Route path="/renewables" element={<Renewables />} />
+        {/* renewables page child comonents started  */}
+        <Route path="/benefits" element={<Benefits />} />
+
+        {/* renewables page child comonents finished  */}
         <Route path="/newroom" element={<Newsroom />} />
         <Route path="/publications" element={<Publication />} />
         <Route path="/education" element={<Education />} />
--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AboutLanding from "../../components/AboutLanding/AboutLanding";
 import CompanyMembers from "../../components/CompanyMembers/CompanyMembers";
 import EnergySources from "../../components/EnergySources/EnergySources";
-import HeroSlider from "../../components/HeroSlider/HeroSlider";
+// import HeroSlider from "../../components/HeroSlider/HeroSlider";
 import IntroLanding from "../../components/IntroLanding/IntroLanding";
 import NewsLanding from "../../components/NewsLanding/NewsLanding";
 import PublicationLanding from "../../components/PublicationLanding/PublicationLanding";
