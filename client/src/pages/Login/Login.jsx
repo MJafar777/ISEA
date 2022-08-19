a8aed5366a4c05605f2ffa85338dedfa3f7d1e4a
Pulatoff <niyozbekpulatov@gmail.com>
2022-08-19 11:34:07 +0500

    jovohi

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,16 +1,24 @@
 import axios from "axios";
 import React, { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import { url } from "../../configs/config";
 import s from "./login.module.css";
 
 export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const handlerLogin = (e) => {
+  const navigate = useNavigate();
+  const handlerLogin = async (e) => {
     e.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-    const response = axios.post(url + "/users/login", { email, password });
+    const response = await axios.post(url + "/users/login", {
+      email,
+      password,
+    });
+    response.data.status === "succes"
+      ? navigate("/")
+      : alert("Bunday user akkaunti tiplmadi,Iltimos qayta urinib kurin");
   };
   return (
     <div className={s.container}>
