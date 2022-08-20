30a649650a7e1a816b2af25a8019074c06c2093b
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-20 15:56:07 +0500

    niyozbek

--- client/src/components/EngineeringComponents/Shablon2/Shablon2.jsx
+++ client/src/components/EngineeringComponents/Shablon2/Shablon2.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "../../Title/Title";
 import a from "./shablon2.module.css";
-
+import imgTitle from "../../../img/factory/9.jpg";
 const Shablon2 = (img, title, subtitle) => {
   const cont = "container" + " " + a.titleSection;
   return (
@@ -9,10 +9,19 @@ const Shablon2 = (img, title, subtitle) => {
       <div className={cont}>
         <div className={a.leftSection}>
           <h1>Title</h1>
-          <div>
-            {/* <h2>{title} title1</h2> */}
-            <h2> title1</h2>
-            {/* <img src="" alt="" /> */}
+          <h2> title1</h2>
+          <div className={a.content}>
+            <img className={a.imgTitle} src={imgTitle} alt="imgTitle" />
+            <p className={a.text}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+              architecto dolor inventore illo animi incidunt fugit. Minima sint,
+              blanditiis repellat impedit dicta tempore, aliquam nam vel tempora
+              voluptas a similique! Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Nisi accusamus porro quam commodi illum at a
+              eos, delectus labore dignissimos aut odio, nemo corporis, iure
+              eius ratione expedita! Modi impedit earum nihil natus numquam
+              assumenda veritatis voluptatum a qui incidunt.
+            </p>
           </div>
         </div>
         <div className={a.rightSection}></div>
