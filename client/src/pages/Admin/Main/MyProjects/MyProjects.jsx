430942de0372ba07113f0f1f26512366fca8ad96
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-08-27 18:10:27 +0500

    S

--- client/src/pages/Admin/Main/MyProjects/MyProjects.jsx
+++ client/src/pages/Admin/Main/MyProjects/MyProjects.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 
+import s from "./projects.module.css";
+
 const MyProjects = () => {
-  return <div>My Projects</div>;
+  return (
+    <>
+      <div className={s.header}>
+        <h2 className={s.title}>Projects</h2>
+        <div className={s.plus}>
+          <p to={"#addusers"}>+</p>
+        </div>
+      </div>
+      <div className={s.input}>
+        <form action="" className={s.form}>
+          <input type="text" name="title" placeholder="Title" />
+          <input type="text" name="category" placeholder="Category" />
+          <input
+            type="text"
+            name="sub_description"
+            placeholder="Project haqida qisqacha malumot"
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="Project haqida toliq malumot"
+          />
+          <label for="file1">Iltimos project rasmini kiriting</label>
+          <input
+            type="file"
+            name="projectImage"
+            id="file1"
+            placeholder="Photoni kiriting"
+            accept="image/"
+          />
+          <label for="file2">Iltimos project presentatsiyasini kiriting</label>
+          <input
+            type="file"
+            name="presentation"
+            id="file2"
+            placeholder="Presentatsiyani kiriting"
+          />
+
+          <select name="category" id="category">
+            <option value="articles">Articles</option>
+            <option value="presses">Presses</option>
+            <option value="experts">Experts</option>
+          </select>
+          <input type="text" name="publisher" placeholder="Publisher" />
+          <textarea type="text" name="description" placeholder="Description" />
+          <button type="submit">ADD</button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default MyProjects;
