ccb604fb5f7ae1019e7544780b9bd59665c43d7f
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-08-27 17:29:46 +0500

    s

--- client/src/pages/Admin/Main/Projects/Projects.jsx
+++ client/src/pages/Admin/Main/Projects/Projects.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import ProjectCard from "../../../../components/ProjectCard/ProjectCard";
+
+import s from "./projects.module.css";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <>
+      <div className={s.header}>
+        <h2 className={s.title}>Projects</h2>
+      </div>
+      <div className={s.pro}>
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    </>
+  );
 }
