d735255b3c55ebacfcc1d10cbfc0b6392feef80d
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-31 14:45:34 +0500

    qwerty

--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -27,7 +27,7 @@ const HomePage = () => {
         {/* <HeroSlider /> */}
         <IntroLanding />
         <AboutLanding />
-        {/* <NewsLanding /> */}
+        <NewsLanding />
       </div>
       <PublicationLanding />
       <CompanyMembers />
