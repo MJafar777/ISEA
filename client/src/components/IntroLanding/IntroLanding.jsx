763662e11e7c585348427a422e04014c1c77dccc
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-31 13:29:11 +0500

    hero video finished

--- client/src/components/IntroLanding/IntroLanding.jsx
+++ client/src/components/IntroLanding/IntroLanding.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 import s from "./introLanding.module.css";
 
-import img1 from "../../img/MainNew/EuroSun.jpg";
-import img2 from "../../img/MainNew/InCore.jpg";
-import img3 from "../../img/MainNew/photovoltaic.jpg";
-import img4 from "../../img/MainNew/SWC_YearOf.jpg";
+// import img1 from "../../img/MainNew/EuroSun.jpg";
+import img1 from "../../img/introLanding/1.jpg";
+// import img2 from "../../img/MainNew/InCore.jpg";
+import img2 from "../../img/introLanding/2.jpg";
+// import img3 from "../../img/MainNew/photovoltaic.jpg";
+import img3 from "../../img/introLanding/3.jpg";
+// import img4 from "../../img/MainNew/SWC_YearOf.jpg";
+import img4 from "../../img/introLanding/4.jpg";
 
 const IntroLanding = () => {
   return (
@@ -19,15 +23,24 @@ const IntroLanding = () => {
       </div>
       <div className={s.cardLevel}>
         <img src={img2} alt="img2" />
-        <p>ISES Solar Energy Museum + SWC 50 Celebratory Booklet</p>
+        <p>
+          ISES Solar Energy Museum + SWC 50 Celebratory Booklet Education on
+          Renewable Energies
+        </p>
       </div>
       <div className={s.cardLevel}>
         <img src={img3} alt="img3" />
-        <p>EuroSun 2022 Education on Renewable Energies</p>
+        <p>
+          EuroSun 2022 Education on Renewable Energies Education on Renewable
+          Energies
+        </p>
       </div>
       <div className={s.cardLevel}>
         <img src={img4} alt="img4" />
-        <p>Webinars ISES Solar Energy Museum + SWC 50 Celebratory Booklet</p>
+        <p>
+          Webinars ISES Solar Energy Museum + SWC 50 Celebratory Booklet
+          Education on Renewable Energies
+        </p>
       </div>
     </div>
   );
