497f735837185ddc183148a5d3ecd0602e675fc2
jafar <mirzaraximovjafar2001@gmail.com>
2022-09-01 20:08:19 +0500

    change images for avif

--- client/src/components/RenewablesComponents/BENEFITSHEATING-COOLING/Benefitsheating.jsx
+++ client/src/components/RenewablesComponents/BENEFITSHEATING-COOLING/Benefitsheating.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import Title from "../../Title/Title";
 import Shablon from "../ShablonRenewables/Shablon";
 
-import img1 from "../../../img/hero-Slider/1.jpg";
-import img2 from "../../../img/hero-Slider/2.jpg";
-import img3 from "../../../img/hero-Slider/3.jpg";
-import img4 from "../../../img/hero-Slider/4.jpg";
-import img5 from "../../../img/hero-Slider/5.jpg";
-import img6 from "../../../img/hero-Slider/6.jpg";
+import img1 from "../../../img/hero-Slider/1.avif";
+import img2 from "../../../img/hero-Slider/2.avif";
+import img3 from "../../../img/hero-Slider/3.avif";
+import img4 from "../../../img/hero-Slider/4.avif";
+import img5 from "../../../img/hero-Slider/5.avif";
+import img6 from "../../../img/hero-Slider/6.avif";
 
 const Benefitsheating = () => {
   return (
