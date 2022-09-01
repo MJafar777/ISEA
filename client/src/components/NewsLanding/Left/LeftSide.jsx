41ba24b02b9b8f136d22bec9c0658cee3002fe1a
Pulatoff <niyozbekpulatov@gmail.com>
2022-09-01 10:19:03 +0500

    good add

--- client/src/components/NewsLanding/Left/LeftSide.jsx
+++ client/src/components/NewsLanding/Left/LeftSide.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "../NewsLanding.module.css";
 
-export default function LeftSide({ title, image, text, date }) {
+export default function LeftSide({ title, image, text, date, id }) {
   return (
     <div>
       {" "}
       <div className={s.left}>
         <h2 className={s.mainText}>
-          <a href="#">{title}</a>
+          <NavLink to={"/news/" + id}>{title}</NavLink>
         </h2>
         <p className={s.bigdate}>{date}</p>
         <img src={image} className={s.mainImg} alt={title} />
