2d68d4c0b999f3ff3b29c3725520cca01b6fe25e
jafar <mirzaraximovjafar2001@gmail.com>
2022-09-02 12:09:43 +0500

    change about

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Title from "../../components/Title/Title";
 import s from "./AboutPage.module.css";
-import about from "../../img/About/about-isea/top.avif";
+import about from "../../img/About/about-isea/top.jpg";
 import About from "./About/About";
 
 const AboutPage = () => {
   return (
-    <div className={s.tops + " " + "container"}>
-      <Title name={"About ISES"} />
-      <img src={about} alt="batary" className={s.batary} />
-      <div className={s.containerInfo}>
-        <About />
+    <div className={s.AboutPage}>
+      <div className={s.tops + " " + "container"}>
+        <Title name={"About ISES"} />
+        <img src={about} alt="batary" className={s.batary} />
+        <div className={s.containerInfo}>
+          <About />
+        </div>
       </div>
     </div>
   );
