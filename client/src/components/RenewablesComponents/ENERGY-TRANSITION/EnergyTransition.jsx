18af5d89c5c05dc4a398efeee31f03a3b6d2ee3c
jafar <mirzaraximovjafar2001@gmail.com>
2022-09-03 13:49:06 +0500

    Removed unnecessary code in renewables component and removed other images

--- client/src/components/RenewablesComponents/ENERGY-TRANSITION/EnergyTransition.jsx
+++ client/src/components/RenewablesComponents/ENERGY-TRANSITION/EnergyTransition.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 
-import img1 from "../../../img/hero-Slider/1.avif";
-import img2 from "../../../img/hero-Slider/2.avif";
-import img3 from "../../../img/hero-Slider/3.avif";
-import img4 from "../../../img/hero-Slider/4.avif";
-import img5 from "../../../img/hero-Slider/5.avif";
-import img6 from "../../../img/hero-Slider/6.avif";
+
 import Title from "../../Title/Title";
 
 const EnergyTransition = () => {
