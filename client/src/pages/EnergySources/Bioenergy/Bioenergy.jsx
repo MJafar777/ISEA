683d750748e6b81bc2b94a06220459b5871ad86a
jafar <mirzaraximovjafar2001@gmail.com>
2022-09-04 15:42:25 +0500

    change

--- client/src/pages/EnergySources/Bioenergy/Bioenergy.jsx
+++ client/src/pages/EnergySources/Bioenergy/Bioenergy.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import EnergySourcesShablon from "../../../components/EnergySources/EnergySourcesShablon/EnergySourcesShablon";
+import Title from "../../../components/Title/Title";
 
 const Bioenergy = () => {
-  return <div className="container">Bioenergy</div>;
+  
+  const content1 = "";
+  const content2 = "";
+  const content3 = "";
+  const content4 = "";
+  return (
+    <div className="container">
+      <Title name={"Bioenergy"} />
+      <EnergySourcesShablon
+        content1={content1}
+        // img1={img1}
+        // img2={img2}
+        content2={content2}
+      />
+      <EnergySourcesShablon
+        content1={content3}
+        // img1={img3}
+        // img2={img4}
+        content2={content4}
+      />
+    </div>
+  );
 };
 
 export default Bioenergy;
