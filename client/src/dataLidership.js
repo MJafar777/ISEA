20aa1386cf4676e5baf619ba750f6b385bbbbbab
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-09-05 21:07:14 +0500

    s

---
+++ client/src/dataLidership.js
@@ -0,0 +1,29 @@
+import img1 from "./img/ISEA_Lidership/jafar.avif";
+import img2 from "./img/ISEA_Lidership/niyozbek.avif";
+import img3 from "./img/ISEA_Lidership/mamur.avif";
+
+const obj = [
+  {
+    id: 1,
+    name: "Jafar Mirzaraximov",
+    status: "Team Leader",
+    about: "Jafar",
+    img: img1,
+  },
+  {
+    id: 2,
+    name: "Niyozbek Polatov",
+    status: "Full-Stack Developer",
+    about: "Niyozbek",
+    img: img2,
+  },
+  {
+    id: 3,
+    name: "Mamurjon Ibragimov",
+    status: "Full-Stack Developer",
+    about: "Mamurjon",
+    img: img3,
+  },
+];
+
+export default obj;
