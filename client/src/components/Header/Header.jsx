5749dea5d45d5ece7455e396e99c241b98a6381b
jafar <mirzaraximovjafar2001@gmail.com>
2022-09-07 15:05:28 +0500

    qwerty

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -198,13 +198,13 @@ const Header = () => {
                   </div>
                 </div>
                 {/* <div className={s.selector}>
-             <img className={s.lang} src={lang} alt="lang" />
-            <select className="select" name="language" id="language">
-              <option value="eng">Eng</option>
-              <option value="rus">Rus</option>
-              <option value="uzb">Uzb</option>
-            </select> 
-          </div> */}
+                  <img className={s.lang} src={lang} alt="lang" />
+                  <select className="select" name="language" id="language">
+                    <option value="eng">Eng</option>
+                    <option value="rus">Rus</option>
+                    <option value="uzb">Uzb</option>
+                  </select>
+                </div> */}
               </div>
             </div>
           </div>
