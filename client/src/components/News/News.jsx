b36fde1617308e811d5681e5cbf8bb96048a2022
Jafar <mirzaraximovjafar2001@gmail.com>
2022-09-24 15:51:34 +0500

    news popular news components started

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import s from "./news.module.css";
 
+import img from "../../img/New/11.avif";
 const News = () => {
-  return <div>parot</div>;
+  return (
+    <div className={s.card}>
+      <div className={s.news_parent__home}>
+        <div className={s.latest_news}>
+          <div className={s.title}>
+            <h2 className={s.first_title}>Latest News</h2>
+          </div>
+          <div className={s.render_news}>
+            <div className={s.news_left}>
+              <img className={s.news_left__img} src={img} alt="#" />
+              <h4 className={s.news_left__img_title}>
+                As a part of its contribution to the Climate Investment Platform
+              </h4>
+              <p className={s.news_left__img_content}>
+                As a part of its contribution to the Climate Investment
+                Platform, ISEA invites financial institutions, development
+                organisations
+              </p>
+              <div className={s.title_data}>
+                <h5>ISES Solar Energy Museum</h5>
+                <h3>12/12/21</h3>
+              </div>
+            </div>
+            <div className={s.news_left}>
+              <img className={s.news_left__img} src={img} alt="#" />
+              <h4 className={s.news_left__img_title}>
+                As a part of its contribution to the Climate Investment Platform
+              </h4>
+              <p className={s.news_left__img_content}>
+                As a part of its contribution to the Climate Investment
+                Platform, ISEA invites financial institutions, development
+                organisations
+              </p>
+              <div className={s.title_data}>
+                <h5>ISES Solar Energy Museum</h5>
+                <h3>12/12/21</h3>
+              </div>
+            </div>
+            <div className={s.news_left}>
+              <img className={s.news_left__img} src={img} alt="#" />
+              <h4 className={s.news_left__img_title}>
+                As a part of its contribution to the Climate Investment Platform
+              </h4>
+              <p className={s.news_left__img_content}>
+                As a part of its contribution to the Climate Investment
+                Platform, ISEA invites financial institutions, development
+                organisations
+              </p>
+              <div className={s.title_data}>
+                <h5>ISES Solar Energy Museum</h5>
+                <h3>12/12/21</h3>
+              </div>
+            </div>
+            <div className={s.news_left}>
+              <img className={s.news_left__img} src={img} alt="#" />
+              <h4 className={s.news_left__img_title}>
+                As a part of its contribution to the Climate Investment Platform
+              </h4>
+              <p className={s.news_left__img_content}>
+                As a part of its contribution to the Climate Investment
+                Platform, ISEA invites financial institutions, development
+                organisations
+              </p>
+              <div className={s.title_data}>
+                <h5>ISES Solar Energy Museum</h5>
+                <h3>12/12/21</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={s.popular_news}>
+          <div className={s.title}>
+            <h2 className={s.first_title}>Popular News</h2>
+          </div>
+          <div className={s.render_popular_news}>
+            <div className={s.popular_news}>
+              <div>
+                <img className={s.popular_news__img} src={img} alt="#" />
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default News;
