7d9c471d3b55dfb8ba2c5e3c8570c1ac73ad684c
Jafar <mirzaraximovjafar2001@gmail.com>
2022-09-24 17:02:50 +0500

    news section front finished and responsive started

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -73,16 +73,108 @@ const News = () => {
             </div>
           </div>
         </div>
+
+        {/*  */}
+
         <div className={s.popular_news}>
           <div className={s.title}>
             <h2 className={s.first_title}>Popular News</h2>
           </div>
           <div className={s.render_popular_news}>
-            <div className={s.popular_news}>
-              <div>
+            <div className={s.popular_news_card}>
+              <div className={s.popular_news_card__left}>
                 <img className={s.popular_news__img} src={img} alt="#" />
               </div>
-              <div></div>
+              <div className={s.popular_news_card__right}>
+                <h4 className={s.popular_news_card__right_title}>
+                  The main purpose of the activity: training and retraining of
+                  personnel
+                </h4>
+                <p className={s.popular_news_card__right_text}>
+                  supporting the effective use of advanced technologies in the
+                  field of solar energy.
+                </p>
+                <div className={s.title_data}>
+                  <h5>ISES Solar Energy Museum</h5>
+                  <h3 className={s.date}>12/12/21</h3>
+                </div>
+              </div>
+            </div>
+            <div className={s.popular_news_card}>
+              <div className={s.popular_news_card__left}>
+                <img className={s.popular_news__img} src={img} alt="#" />
+              </div>
+              <div className={s.popular_news_card__right}>
+                <h4 className={s.popular_news_card__right_title}>
+                  The main purpose of the activity: training and retraining of
+                  personnel
+                </h4>
+                <p className={s.popular_news_card__right_text}>
+                  supporting the effective use of advanced technologies in the
+                  field of solar energy.
+                </p>
+                <div className={s.title_data}>
+                  <h5>ISES Solar Energy Museum</h5>
+                  <h3 className={s.date}>12/12/21</h3>
+                </div>
+              </div>
+            </div>
+            <div className={s.popular_news_card}>
+              <div className={s.popular_news_card__left}>
+                <img className={s.popular_news__img} src={img} alt="#" />
+              </div>
+              <div className={s.popular_news_card__right}>
+                <h4 className={s.popular_news_card__right_title}>
+                  The main purpose of the activity: training and retraining of
+                  personnel
+                </h4>
+                <p className={s.popular_news_card__right_text}>
+                  supporting the effective use of advanced technologies in the
+                  field of solar energy.
+                </p>
+                <div className={s.title_data}>
+                  <h5>ISES Solar Energy Museum</h5>
+                  <h3 className={s.date}>12/12/21</h3>
+                </div>
+              </div>
+            </div>
+            <div className={s.popular_news_card}>
+              <div className={s.popular_news_card__left}>
+                <img className={s.popular_news__img} src={img} alt="#" />
+              </div>
+              <div className={s.popular_news_card__right}>
+                <h4 className={s.popular_news_card__right_title}>
+                  The main purpose of the activity: training and retraining of
+                  personnel
+                </h4>
+                <p className={s.popular_news_card__right_text}>
+                  supporting the effective use of advanced technologies in the
+                  field of solar energy.
+                </p>
+                <div className={s.title_data}>
+                  <h5>ISES Solar Energy Museum</h5>
+                  <h3 className={s.date}>12/12/21</h3>
+                </div>
+              </div>
+            </div>
+            <div className={s.popular_news_card}>
+              <div className={s.popular_news_card__left}>
+                <img className={s.popular_news__img} src={img} alt="#" />
+              </div>
+              <div className={s.popular_news_card__right}>
+                <h4 className={s.popular_news_card__right_title}>
+                  The main purpose of the activity: training and retraining of
+                  personnel
+                </h4>
+                <p className={s.popular_news_card__right_text}>
+                  supporting the effective use of advanced technologies in the
+                  field of solar energy.
+                </p>
+                <div className={s.title_data}>
+                  <h5>ISES Solar Energy Museum</h5>
+                  <h3 className={s.date}>12/12/21</h3>
+                </div>
+              </div>
             </div>
           </div>
         </div>
