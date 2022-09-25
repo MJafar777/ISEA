92f66e911d2812f737a505ca4e0cd3f04b25c506
bekzod <bekzodbaratov@gmail.com>
2022-09-25 19:49:36 +0500

    hero section fixed

--- client/src/components/HeaderVideo/HeaderVideo.jsx
+++ client/src/components/HeaderVideo/HeaderVideo.jsx
@@ -7,16 +7,14 @@ const HeaderVideo = () => {
   return (
     <div className={s.headerVideo}>
       <video autoPlay muted loop className={s.myVideo}>
-        <source src={headerVideo} type="video/mp4" />
+        <source className={s.heroVideo} src={headerVideo} type="video/mp4" />
       </video>
       <div className={s.content}>
         <div className={s.contentChild}>
           <h1 className={s.title}>ISEA ASSOCIATION</h1>
           <p className={s.description}>
-            The main purpose of the activity: training and retraining of
-            personnel at the international level in the field of solar energy,
-            supporting the effective use of advanced technologies in the field
-            of solar energy.
+            The main purpose of the activity: training and retraining of personnel at the international level in the
+            field of solar energy, supporting the effective use of advanced technologies in the field of solar energy.
           </p>
           <button className={s.button}>Read more </button>
         </div>
