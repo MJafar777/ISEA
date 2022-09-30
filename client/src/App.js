11fccc15a076f64eb3f994c56450ca00e007ba67
Jafar <mirzaraximovjafar2001@gmail.com>
2022-09-30 16:08:41 +0500

    error page finished

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,6 @@ import Costs from "./components/RenewablesComponents/COSTS/Costs";
 import Power from "./components/RenewablesComponents/POWER/Power";
 import Admin from "./pages/Admin/Admin";
 import Login from "./pages/Login/Login";
-import Shablon2 from "./components/EngineeringComponents/Shablon2/Shablon2";
 import Benefits from "./components/RenewablesComponents/Benefits/Benefits";
 import MyProjects from "./pages/MyProjects/MyProjects";
 import OneProject from "./pages/OneProject/OneProject";
@@ -50,6 +49,7 @@ import Ocean from "./pages/EnergySources/Ocean/Ocean";
 import Solar from "./pages/EnergySources/Solar/Solar";
 import obj from "./dataLidership";
 import PublicationPage from "./pages/Publication2/PublicationPage";
+import Error from "./components/Error/Error";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -78,10 +78,16 @@ const App = () => {
 
           {/* about page child comonents started */}
           <Route path="/isea-leadership" element={<IseaLiderShip />} />
-          <Route path="/isea-leadership/:id" element={<OneLidership obj={obj} />} />
+          <Route
+            path="/isea-leadership/:id"
+            element={<OneLidership obj={obj} />}
+          />
           <Route path="/contact" element={<AboutContactPage />} />
           <Route path="/history" element={<History />} />
-          <Route path="/institutional-structure" element={<InstitutionalStructure />} />
+          <Route
+            path="/institutional-structure"
+            element={<InstitutionalStructure />}
+          />
           <Route path="/isea-membership" element={<IseaMembership />} />
 
           <Route path="/official-documents" element={<OfficialDocuments />} />
@@ -100,9 +106,15 @@ const App = () => {
           <Route path="/benefits" element={<Benefits />} />
           <Route path="/climate-change" element={<ClimateChange />} />
           <Route path="/energy-transition" element={<EnergyTransition />} />
-          <Route path="/finance-and-investment" element={<FinanceInvestment />} />
+          <Route
+            path="/finance-and-investment"
+            element={<FinanceInvestment />}
+          />
           <Route path="/heating-cooling" element={<HeatingColing />} />
-          <Route path="/innovation-technology" element={<InnovationTechnology />} />
+          <Route
+            path="/innovation-technology"
+            element={<InnovationTechnology />}
+          />
           <Route path="/inductry-transport" element={<InductryTransport />} />
           <Route path="/costs" element={<Costs />} />
           <Route path="/power" element={<Power />} />
@@ -131,7 +143,6 @@ const App = () => {
           <Route path="/engineering" element={<Engineering />} />
 
           {/* engineering page child comonents started */}
-          <Route path="/shablon2" element={<Shablon2 />} />
           {/* engineering page child comonents finished */}
 
           {/* ---------------------------- */}
@@ -166,6 +177,7 @@ const App = () => {
           <Route path={"/hydopower"} element={<Hydopower />} />
           <Route path={"/ocean"} element={<Ocean />} />
           <Route path={"/solar"} element={<Solar />} />
+          <Route path={"*"} element={<Error />} />
 
           {/* energy sources childrens */}
         </Routes>
---
+++ client/src/components/Error/Error.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import s from "./Error.module.css";
+const Error = () => {
+  return <div className={s.error}></div>;
+};
+
+export default Error;
